811a16a6ec44b9d4f68e3053be71d2fb23c12298
Nasir1290 <nasirmia12901290@gmail.com>
2024-09-08 00:35:29 +0600

    initialize git

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import NavBar from "../shared/navbar/NavBar";
+import TopNavbar from "../shared/navbar/TopNavbar";
+
+const HomePage = () => {
+  return (
+    <div>
+      <TopNavbar />
+      <NavBar />
+    </div>
+  );
+};
+
+export default HomePage;
